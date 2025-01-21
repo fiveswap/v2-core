70665f9d8454761aa384c67291a3cb127a08f678
syed mohib <syedMohib44>
2025-01-21 06:36:51 +0500

    Create NPM package also changed version of TransferHelper.sol

--- contracts/libraries/TransferHelper.sol
+++ contracts/libraries/TransferHelper.sol
@@ -1,14 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity >=0.6.0;
+pragma solidity =0.5.16;
 
 // helper methods for interacting with ERC20 tokens and sending PEN that do not consistently return true/false
 library TransferHelper {
-    function safeApprove(
-        address token,
-        address to,
-        uint256 value
-    ) internal {
+    function safeApprove(address token, address to, uint256 value) internal {
         // bytes4(keccak256(bytes('approve(address,uint256)')));
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x095ea7b3, to, value));
         require(
@@ -17,11 +13,7 @@ library TransferHelper {
         );
     }
 
-    function safeTransfer(
-        address token,
-        address to,
-        uint256 value
-    ) internal {
+    function safeTransfer(address token, address to, uint256 value) internal {
         // bytes4(keccak256(bytes('transfer(address,uint256)')));
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
         require(
@@ -30,12 +22,7 @@ library TransferHelper {
         );
     }
 
-    function safeTransferFrom(
-        address token,
-        address from,
-        address to,
-        uint256 value
-    ) internal {
+    function safeTransferFrom(address token, address from, address to, uint256 value) internal {
         // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
         require(
@@ -45,7 +32,7 @@ library TransferHelper {
     }
 
     function safeTransferPEN(address to, uint256 value) internal {
-        (bool success, ) = to.call{value: value}(new bytes(0));
+        (bool success, ) = to.call.value(value)(new bytes(0));
         require(success, 'TransferHelper::safeTransferPEN: PEN transfer failed');
     }
 }
