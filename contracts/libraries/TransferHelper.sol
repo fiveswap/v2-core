9a1246fbce3cf9f111c3bd7d363f02db428797fb
Absarsdq291 <absarsdq291@gmail.com>
2025-02-12 04:11:52 +0500

    .

--- contracts/libraries/TransferHelper.sol
+++ contracts/libraries/TransferHelper.sol
@@ -43,7 +43,7 @@ library TransferHelper {
         );
     }
 
-    function safeTransferETH(address to, uint256 value) internal {
+    function safeTransferPEN(address to, uint256 value) internal {
         (bool success, ) = to.call{value: value}(new bytes(0));
         require(success, 'TransferHelper::safeTransferETH: ETH transfer failed');
     }
